d0976d8995426cd00d52206c158635d91fe4368e
rishijadhav211 <rishijadhav211@gmail.com>
2022-01-27 16:47:17 +0530

    about page change

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -4,7 +4,7 @@ import TitleBar from "../components/TitleBar";
 import Card from "react-bootstrap/Card";
 import Logoimg from "../images/logoRed.png";
 import {Row,Col } from "react-bootstrap";
-import {SiLinkedin} from "react-icons/si";
+import {SiLinkedin,SiGmail} from "react-icons/si";
 import { FcGoogle} from "react-icons/fc";
 
 function ViewAsset() {
@@ -31,16 +31,16 @@ function ViewAsset() {
            
             <hr></hr>
             <p style={{textAlign:"center",fontSize:"20px"}}><b>&nbsp; &nbsp; Developers :</b></p>
-            <Row style={{textAlign:"center",marginLeft:"20%",marginRight:"20%"}} >
+          <Row style={{textAlign:"center",margin:"0",padding:"0"}} >
           
            <Col md={6}>
-           <div className="card">
+           <div className="card1" >
             
             <h3 style={{marginTop:"10px"}}>Pranav Bhosale</h3>
             <div className="mb-2 text-muted">2019BTEIT00038</div>
             <Row>
-            <Col style={{marginLeft:"30%"}}><div className="iconn"> <a href="https://www.linkedin.com/in/pranav-bhosale"><SiLinkedin/></a></div></Col>
-            <Col style={{marginRight:"30%"}}><div className="iconn"><a href="mailto:pranav.b.bhosale@gmail.com"><FcGoogle/></a></div></Col>
+            <Col style={{marginLeft:"30%",paddingBottom:"5px"}}><div className="iconn"> <a href="https://www.linkedin.com/in/pranav-bhosale"><SiLinkedin/></a></div></Col>
+            <Col style={{marginRight:"30%",paddingBottom:"5px"}}><div className="iconn"><a href="mailto:pranav.b.bhosale@gmail.com"><SiGmail/></a></div></Col>
             </Row>
             
             </div>
@@ -48,18 +48,33 @@ function ViewAsset() {
 
 
             <Col md={6}>
-            <div className="card">
+            <div className="card2" >
             
             <h3 style={{marginTop:"10px"}}>Rushikesh Jadhav</h3>
             <div className="mb-2 text-muted">2020BTEIT00206</div>
             <Row>
-            <Col style={{marginLeft:"30%"}}><div className="iconn"> <a href="https://www.linkedin.com/in/rushikesh-jadhav211"><SiLinkedin/></a></div></Col>
-            <Col style={{marginRight:"30%"}}><div className="iconn"><a href="mailto:rishijadhav211@gmail.com"><FcGoogle/></a></div></Col>
+            <Col style={{marginLeft:"30%",paddingBottom:"5px"}}><div className="iconn"> <a href="https://www.linkedin.com/in/rushikesh-jadhav211"><SiLinkedin/></a></div></Col>
+            <Col style={{marginRight:"30%",paddingBottom:"5px"}}><div className="iconn"><a href="mailto:rishijadhav211@gmail.com"><SiGmail/></a></div></Col>
             </Row>
             
             </div>
             </Col>
           </Row>
+          <br></br>
+          <div style={{textAlign:"center"}}>
+          <Row style={{textAlign:"center",margin:"0",padding:"0"}} >
+          <div className="card" style={{width:"300px",margin:"auto"}}>
+            
+            <h3 style={{marginTop:"10px"}}>Swarangi Patil</h3>
+            <div className="mb-2 text-muted">2019BTEIT00056</div>
+            <Row style={{textAlign:"center"}}>
+            <Col style={{marginLeft:"30%",paddingBottom:"5px"}}><div className="iconn"> <a href="https://www.linkedin.com/in/swarangi-patil"><SiLinkedin/></a></div></Col>
+            <Col style={{marginRight:"30%",paddingBottom:"5px"}}><div className="iconn"><a href="mailto:swarangi.patil@gmail.com"><SiGmail/></a></div></Col>
+            </Row>
+            
+            </div>
+          </Row>
+          </div>
         
         <Footer />
     </div>
