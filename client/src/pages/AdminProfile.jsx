8062204b9c6236f8468231e17d2946dcdf5d1d0e
rishijadhav211 <rishijadhav211@gmail.com>
2022-04-01 23:54:02 +0530

    modal added to manage admin/add admin

--- client/src/pages/AdminProfile.jsx
+++ client/src/pages/AdminProfile.jsx
@@ -1,16 +1,120 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import{Row,Col,Container,Modal,Button} from "react-bootstrap";
 
 function ViewAsset() {
- 
+
+  const [show, setShow] = React.useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [showChangePass, setShowChangePass] = React.useState(false);
+  const handleCloseforchangePass = () => setShowChangePass(false);
+  const handleShowchangePass = () => setShowChangePass(true);
+  
+  function dummyforchangepass(){
+    setShowChangePass(false);
+    console.log("success!")
+  }
   return (
+
     <div className="navfootpad">
-        <Navbar />
-         <div>
-        admin profile
-         </div>
-        <Footer />
+
+<Modal show={show} onHide={handleClose}>
+    <Modal.Header closeButton>
+      <Modal.Title>Loging Out</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>Do you really want to Logout!</Modal.Body>
+    <Modal.Footer>
+      <Button variant="danger" onClick={handleClose}>
+        Logout
+      </Button>
+    </Modal.Footer>
+  </Modal>
+
+
+  <Modal show={showChangePass} onHide={handleCloseforchangePass}>
+    <Modal.Header closeButton>
+      <Modal.Title>Change Password</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>Password reset link will be sent on registered email!</Modal.Body>
+    <Modal.Footer>
+      <Button variant="warning" onClick={dummyforchangepass}>
+        Send Link
+      </Button>
+    </Modal.Footer>
+  </Modal>
+
+      <Navbar />
+     
+      <h1
+                style={{
+                  textAlign: "center",
+                  paddingLeft: "0",
+                  paddingRight: "0",
+                  marginRight: "0",
+                }}
+              >
+              Profile
+              </h1>
+              <hr></hr>
+              <div
+              
+              >
+        <Container
+         style={{
+                    paddingLeft: "20px",
+                    paddingRight: "20px",
+                    alignContent: "center",
+                    marginTop: "50px",
+                    border: "1px solid",
+                  }}
+        >
+        <br></br>
+          <Row className="justify-content-md-center">
+            <Col md={{ span: 3, offset: 4}}>
+              User Name :
+            </Col>
+            <Col md={{ span: 5, offset: 0}}>
+            <p style={{color:"#707070"}}> rishijadhav211@gmail.com</p>
+            </Col>
+          </Row>
+          <Row className="justify-content-md-center">
+            <Col md={{ span: 3, offset: 4}}>
+             Email :
+            </Col>
+            <Col md={{ span: 5, offset: 0}} >
+              <p style={{color:"#707070"}}> rishijadhav211@gmail.com</p>
+            </Col>
+          </Row>
+          <Row className="justify-content-md-center">
+            <Col md={{ span: 3, offset: 4}}>
+              Department :
+            </Col>
+            <Col md={{ span: 5, offset: 0}} >
+              <p style={{color:"#707070"}}> rishijadhav211@gmail.com</p>
+            </Col>
+          </Row>
+          <br></br>
+    
+          <Row className="justify-content-md-center">
+            <Col md={{ span: 3, offset: 3}} style={{textAlign:"left" }}>
+            <button className="lanButton" onClick={handleShow} >
+                        Logout
+                      </button>
+            </Col>
+            <Col md={{ span: 4, offset: 0}} style={{textAlign:"left" }}>
+            <button className="lanButton" onClick={handleShowchangePass} >
+                        Change Password
+                      </button>
+            </Col>
+          </Row>
+
+        </Container>
+      </div>
+      <Footer />
     </div>
   );
 }
